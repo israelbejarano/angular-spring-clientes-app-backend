c164048492540707257af211709a16d9a26408a6
israel <israel.bejarano.a@gmail.com>
2020-01-16 20:07:53 +0100

    servicio cliente con find all

---
+++ spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/services/IClienteService.java
@@ -0,0 +1,11 @@
+package com.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import com.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteService {
+	
+	public List<Cliente> findAll();
+
+}
