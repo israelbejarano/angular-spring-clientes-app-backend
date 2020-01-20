bd9a9e8dc958566923990ec5766744778d50c934
israel <israel.bejarano.a@gmail.com>
2020-01-20 16:50:30 +0100

    ahora el front añade la fecha de creacion

--- spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/entity/Cliente.java
+++ spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/entity/Cliente.java
@@ -8,13 +8,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
+// import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
 
 @Entity
 @Table(name = "clientes")
@@ -40,13 +42,14 @@ public class Cliente implements Serializable {
 	private String email;
 	
 	@Column(name = "create_at")
+	@NotNull(message = "no puede estar vacío")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
 	
-	@PrePersist
+	/*@PrePersist
 	public void prePersist() {
 		createAt = new Date();
-	}
+	}*/
 
 	public Long getId() {
 		return id;
