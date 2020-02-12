e1a24b27c8e34dd641d5b9d1f816951875273ac0
israel <israel.bejarano.a@gmail.com>
2020-02-12 13:05:42 +0100

    relacion de region con clientes

--- spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/entity/Cliente.java
+++ spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/entity/Cliente.java
@@ -5,9 +5,12 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 // import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -17,41 +20,56 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "clientes")
 public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false)
 	@NotEmpty(message = "no puede estar vacío")
 	@Size(min = 3, max = 30)
 	private String nombre;
-	
+
 	@NotEmpty
 	private String apellido;
-	
+
 	@NotEmpty
 	@Email
 	@Column(nullable = false, unique = true)
 	private String email;
-	
+
 	@Column(name = "create_at")
 	@NotNull(message = "no puede estar vacío")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
-	
+
 	private String foto;
-	
-	/*@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}*/
+
+	@ManyToOne(fetch = FetchType.LAZY) // carga perezosa
+	@JoinColumn(name = "region_id")
+	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" }) // al usar carga perezosa tenemos que ignorar estas
+																		// dos propiedades del json mapeado ya que si no
+																		// nos dará error
+	private Region region;
+
+	/*
+	 * @PrePersist public void prePersist() { createAt = new Date(); }
+	 */
+
+	public Region getRegion() {
+		return region;
+	}
+
+	public void setRegion(Region region) {
+		this.region = region;
+	}
 
 	public String getFoto() {
 		return foto;
