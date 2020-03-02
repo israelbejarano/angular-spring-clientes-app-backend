b3a6dfe7bf05f90d18ceb1d4fd3694c7b5e52e56
israel <israel.bejarano.a@gmail.com>
2020-03-02 13:46:52 +0100

    authenticationManager method

--- spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/auth/SpringSecurityConfig.java
+++ spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/auth/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.springboot.backend.apirest.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,10 +11,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private UserDetailsService usuarioService;
-	
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -24,7 +25,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());
 	}
-	
-	
+
+	@Override
+	@Bean("authenticationManager")
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 
 }
