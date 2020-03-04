205a4dbc7e7d3128bec74f0dc68e8efe5d086e7c
israel <israel.bejarano.a@gmail.com>
2020-03-04 20:11:27 +0100

    generar password encryptados

--- spring-boot-backend-apirest/src/main/resources/import.sql
+++ spring-boot-backend-apirest/src/main/resources/import.sql
@@ -20,3 +20,14 @@ INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(3, '
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(5, 'Magma', 'Lee', 'magma.lee@gmail.com', '2020-03-04');
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(6, 'Tornado', 'Roe', 'tornado.roe@gmail.com', '2020-03-05');
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(7, 'Jade', 'Doe', 'jane.doe@gmail.com', '2020-03-06');
+
+/* Creamos algunos usuarios con sus roles */
+INSERT INTO `usuarios` (username, password, enabled) VALUES ('israel','$2a$10$eU0ZGxBvaWREnzYb3sReTuyXOFGfsrwEi7GjkO7xfFx9QcOI8q4gy',1);
+INSERT INTO `usuarios` (username, password, enabled) VALUES ('admin','$2a$10$wjJ39MB0f2wEpqbIiyKbieEnX17zkpJ6fiNeYi8D2jMBm0dbJ0bvG',1);
+
+INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
+INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 1);
