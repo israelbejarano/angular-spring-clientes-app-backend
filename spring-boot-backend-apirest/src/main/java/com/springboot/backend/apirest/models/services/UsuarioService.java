54cadfb67e7b949e417bba21bb5e1cfef7f05191
israel <israel.bejarano.a@gmail.com>
2020-03-05 13:03:50 +0100

    Añadiendo más datos en el token JWT

--- spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/services/UsuarioService.java
+++ spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/services/UsuarioService.java
@@ -19,7 +19,7 @@ import com.springboot.backend.apirest.models.dao.IUsuarioDao;
 import com.springboot.backend.apirest.models.entity.Usuario;
 
 @Service
-public class UsuarioService implements UserDetailsService {
+public class UsuarioService implements IUsuarioService, UserDetailsService {
 	
 	private Logger logger = LoggerFactory.getLogger(UsuarioService.class);
 	
@@ -40,6 +40,11 @@ public class UsuarioService implements UserDetailsService {
 				.peek(authority -> logger.info("Role: " + authority.getAuthority()))
 				.collect(Collectors.toList());
 		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true, authorities);
+	}
+
+	@Override
+	public Usuario findByUsername(String username) {
+		return usuarioDao.findByUsername(username);
 	}
 
 }
