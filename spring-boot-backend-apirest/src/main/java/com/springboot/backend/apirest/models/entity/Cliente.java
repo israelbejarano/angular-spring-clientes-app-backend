c5a930a2f7f3a64d8922102bdb4f3dab29b3663b
israel <israel.bejarano.a@gmail.com>
2020-03-19 11:09:14 +0100

    relacion de factura con cliente

--- spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/entity/Cliente.java
+++ spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/entity/Cliente.java
@@ -1,8 +1,11 @@
 package com.springboot.backend.apirest.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 // import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -61,9 +65,18 @@ public class Cliente implements Serializable {
 	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 	private Region region;
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cliente", cascade = CascadeType.ALL)
+	private List<Factura> facturas;
+	
+	
+
 	/*
 	 * @PrePersist public void prePersist() { createAt = new Date(); }
 	 */
+
+	public Cliente() {
+		this.facturas = new ArrayList<Factura>();
+	}
 
 	public Region getRegion() {
 		return region;
@@ -119,6 +132,14 @@ public class Cliente implements Serializable {
 
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
 	}
 
 }
