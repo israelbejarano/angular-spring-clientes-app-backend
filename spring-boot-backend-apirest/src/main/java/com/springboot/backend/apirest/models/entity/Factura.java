87c7ea59b5d352c1e2ed40709cacf44fcfc7d734
israel <israel.bejarano.a@gmail.com>
2020-03-19 11:29:49 +0100

    relacion entre facturas y sus itemsfacturas

--- spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/entity/Factura.java
+++ spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/entity/Factura.java
@@ -1,15 +1,20 @@
 package com.springboot.backend.apirest.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -33,6 +38,14 @@ public class Factura implements Serializable {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "factura_id") // necesario para generar la llave foranea en la tabla facturas_items
+	private List<ItemFactura> items;
+
+	public Factura() {
+		items = new ArrayList<ItemFactura>();
+	}
 
 	@PrePersist
 	public void prePersist() {
@@ -77,6 +90,14 @@ public class Factura implements Serializable {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
 	}
 
 }
