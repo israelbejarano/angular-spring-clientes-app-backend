3d9c28fe62721b8c00f308856428e5271f47b9f4
israel <israel.bejarano.a@gmail.com>
2020-03-19 11:29:33 +0100

    entidad para las lineas de la factura

---
+++ spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/models/entity/ItemFactura.java
@@ -0,0 +1,35 @@
+package com.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "facturas_items")
+public class ItemFactura implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer cantidad;
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public Double calcularImporte() {
+		return cantidad.doubleValue();
+	}
+
+}
