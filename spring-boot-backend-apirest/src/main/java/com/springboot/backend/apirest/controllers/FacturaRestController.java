335151859b0ff1dad241dbd28516d24309cf030d
israel <israel.bejarano.a@gmail.com>
2020-03-22 11:52:40 +0100

    controlador para traer los productos a buscar

--- spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/controllers/FacturaRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/controllers/FacturaRestController.java
@@ -1,5 +1,7 @@
 package com.springboot.backend.apirest.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.backend.apirest.models.entity.Factura;
+import com.springboot.backend.apirest.models.entity.Producto;
 import com.springboot.backend.apirest.models.services.IClienteService;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
@@ -31,6 +34,12 @@ public class FacturaRestController {
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		clienteService.deleteFacturaById(id);
+	}
+	
+	@GetMapping("/facturas/filtrar-productos/{term}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public List<Producto> show(@PathVariable String term) {
+		return clienteService.findProductoByNombre(term);
 	}
 
 }
