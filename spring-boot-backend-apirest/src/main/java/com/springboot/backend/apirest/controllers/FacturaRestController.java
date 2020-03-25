66beec430827ba45b7ca8620c885226c2e0fecb7
israel <israel.bejarano.a@gmail.com>
2020-03-25 13:31:38 +0100

    volver a poner la seguridad

--- spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/controllers/FacturaRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/springboot/backend/apirest/controllers/FacturaRestController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,21 +28,31 @@ public class FacturaRestController {
 	private IClienteService clienteService;
 	
 	@GetMapping("/facturas/{id}")
+	@Secured({"ROLE_ADMIN", "ROLE_USER"})
 	@ResponseStatus(code = HttpStatus.OK)
 	public Factura show(@PathVariable Long id) {
 		return clienteService.findFacturaById(id);
 	}
 	
 	@DeleteMapping("/facturas/{id}")
+	@Secured({"ROLE_ADMIN"})
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		clienteService.deleteFacturaById(id);
 	}
 	
 	@GetMapping("/facturas/filtrar-productos/{term}")
+	@Secured({"ROLE_ADMIN"})
 	@ResponseStatus(code = HttpStatus.OK)
-	public List<Producto> show(@PathVariable String term) {
+	public List<Producto> filtrarProductos(@PathVariable String term) {
 		return clienteService.findProductoByNombre(term);
+	}
+	
+	@PostMapping("/facturas")
+	@Secured({"ROLE_ADMIN"})
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Factura crear(@RequestBody Factura factura) {
+		return clienteService.saveFactura(factura);
 	}
 
 }
